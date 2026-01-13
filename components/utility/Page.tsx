9d3c37e43a26863478e24af9efdcd869d281b1bb
sx4im <msaimshafique4664@gmail.com>
2026-01-13 21:52:15 +0500

    change name

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -5,7 +5,7 @@ import Navbar from "../global/Navbar";
 import React, { ReactChildren } from "react";
 
 function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
-  const pageTitle = "Muneeb ul Rehman";
+  const pageTitle = "Muneeb Naeem";
   console.log(currentPage);
   return (
     <div
